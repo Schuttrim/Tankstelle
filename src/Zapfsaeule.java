3165ce41ae61c5f49a57c301830b659651665c27
Schuttrim <lukas.eck@gmx.ch>
2013-11-13 19:28:58 +0100

    Erste Schritte

---
+++ src/Zapfsaeule.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Lukas
+ * Die Klasse der Zapfsaeule
+ *
+ */
+ 
+
+public class Zapfsaeule {
+	// Memberdeklaration
+	public int mNummer;
+	public float mGetankteLiter;
+	public boolean mIstTankbar;
+	public ArrayList<Fuellstotzen> mFuellstotzenliste;
+	// java.util einbinden
+	
+	public Zapfsaeule(){
+		mFuellstotzenliste = new ArrayList<Fuellstotzen>();
+		
+	}
+	
+	public void tanken(float pAnzahlLiter){
+		
+	}
+	
+	public void fuellstotzenAbnehmen(int pNr){
+		
+	}
+	
+	public void fuellstotzenAuflegen(){
+		
+	}
+	
+}
